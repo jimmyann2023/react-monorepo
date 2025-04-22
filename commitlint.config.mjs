a1868cdf27ba6d28d646685904952e078ef09fb1
jimmyann2023 <2492617236@qq.com>
2025-04-22 13:40:57 +0800

    feat: ✨ 增加 script 脚本命令来 lint 和 format代码

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -4,13 +4,69 @@
 
 const userConfig = {
   extends: ['@commitlint/config-conventional'],
+  prompt: {
+    allowBreakingChanges: ['feat', 'fix'],
+    customScopesAlias: 'custom',
+    customScopesAlign: 'bottom',
+    emptyScopesAlias: 'empty',
+    messages: {
+      body: '填写更加详细的变更描述[可选]。使用 "|" 换行 :\n',
+      breaking: '列举非兼容性重大的变更[可选]。使用 "|" 换行 :\n',
+      confirmCommit: '是否提交或修改commit ?',
+      customFooterPrefixs: '输入自定义issue前缀 :',
+      customScope: '请输入自定义的提交范围 :',
+      footer: '列举关联issue [可选] 例如: #31, #I3244 :\n',
+      footerPrefixsSelect: '选择关联issue前缀[可选]:',
+      scope: '选择一个提交范围[可选]:',
+      subject: '请简要描述提交 :\n',
+      type: '选择你要提交的类型 :',
+    },
+    scopes: [],
+    skipQuestions: ['scope', 'body', 'breaking', 'footerPrefix', 'footer'], // 自定义选择指定的问题不显示
+    types: [
+      { emoji: '✨', name: 'feat:   ✨  新增功能', value: 'feat' },
+      { emoji: '🐛', name: 'fix:   🐛  修复缺陷', value: 'fix' },
+      { emoji: '📝', name: 'docs:   📝  文档变更', value: 'docs' },
+      {
+        emoji: '💄',
+        name: 'style:   💄  代码格式',
+        value: 'style',
+      },
+      {
+        emoji: '🔨',
+        name: 'refactor:   🔨  代码重构',
+        value: 'refactor',
+      },
+      { emoji: '⚡️', name: 'perf:    ⚡️  性能优化', value: 'perf' },
+      {
+        emoji: '✅',
+        name: 'test:   ✅  测试',
+        value: 'test',
+      },
+      {
+        emoji: '📦️',
+        name: 'build:   📦️  打包构建',
+        value: 'build',
+      },
+      { emoji: '👷', name: 'ci:   👷  CI 配置变更', value: 'ci' },
+      { emoji: '⏪️', name: 'revert:   ⏪️  代码回退', value: 'revert' },
+      {
+        emoji: '🚀',
+        name: 'chore:   🚀  构建/工程依赖/工具',
+        value: 'chore',
+      },
+      { emoji: '🚧', name: 'wip:   🚧  正在开发中', value: 'wip' },
+      { emoji: '🎯', name: 'workflow:   🎯  工作流程改进', value: 'workflow' },
+    ],
+    useEmoji: true,
+  },
   rules: {
     'body-leading-blank': [2, 'always'],
     'footer-leading-blank': [1, 'always'],
     'header-max-length': [2, 'always', 108],
+    'subject-case': [0],
     'subject-empty': [2, 'never'],
     'type-empty': [2, 'never'],
-    'subject-case': [0],
     'type-enum': [
       2,
       'always',
@@ -32,62 +88,6 @@ const userConfig = {
         'release',
       ],
     ],
-  },
-  prompt: {
-    messages: {
-      type: '选择你要提交的类型 :',
-      scope: '选择一个提交范围[可选]:',
-      customScope: '请输入自定义的提交范围 :',
-      subject: '请简要描述提交 :\n',
-      body: '填写更加详细的变更描述[可选]。使用 "|" 换行 :\n',
-      breaking: '列举非兼容性重大的变更[可选]。使用 "|" 换行 :\n',
-      footerPrefixsSelect: '选择关联issue前缀[可选]:',
-      customFooterPrefixs: '输入自定义issue前缀 :',
-      footer: '列举关联issue [可选] 例如: #31, #I3244 :\n',
-      confirmCommit: '是否提交或修改commit ?',
-    },
-    types: [
-      { value: 'feat', name: 'feat:   ✨  新增功能', emoji: '✨' },
-      { value: 'fix', name: 'fix:   🐛  修复缺陷', emoji: '🐛' },
-      { value: 'docs', name: 'docs:   📝  文档变更', emoji: '📝' },
-      {
-        value: 'style',
-        name: 'style:   💄  代码格式',
-        emoji: '💄',
-      },
-      {
-        value: 'refactor',
-        name: 'refactor:   🔨  代码重构',
-        emoji: '🔨',
-      },
-      { value: 'perf', name: 'perf:    ⚡️  性能优化', emoji: '⚡️' },
-      {
-        value: 'test',
-        name: 'test:   ✅  测试',
-        emoji: '✅',
-      },
-      {
-        value: 'build',
-        name: 'build:   📦️  打包构建',
-        emoji: '📦️',
-      },
-      { value: 'ci', name: 'ci:   👷  CI 配置变更', emoji: '👷' },
-      { value: 'revert', name: 'revert:   ⏪️  代码回退', emoji: '⏪️' },
-      {
-        value: 'chore',
-        name: 'chore:   🚀  构建/工程依赖/工具',
-        emoji: '🚀',
-      },
-      { value: 'wip', name: 'wip:   🚧  正在开发中', emoji: '🚧' },
-      { value: 'workflow', name: 'workflow:   🎯  工作流程改进', emoji: '🎯' },
-    ],
-    useEmoji: true,
-    scopes: [],
-    customScopesAlign: 'bottom',
-    emptyScopesAlias: 'empty',
-    customScopesAlias: 'custom',
-    allowBreakingChanges: ['feat', 'fix'],
-    skipQuestions: ['scope', 'body', 'breaking', 'footerPrefix', 'footer'], // 自定义选择指定的问题不显示
   },
 };
 
