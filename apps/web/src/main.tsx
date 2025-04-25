9cea45b99ec2515f9e281bbb546fbe82c8dd207f
jimmyann2023 <2492617236@qq.com>
2025-04-25 09:43:40 +0800

    fix: 🐛 修复react引入 @repo、shadcn-ui的globals.css未找到

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-// import '@repo/styles/tailwind/theme.css';
 import '@repo/shadcn-ui/globals.css';
 
 import App from './App.tsx';
